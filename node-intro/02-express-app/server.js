fb5b579928c57acc0940583bb984a09026ff9a96
Magesh Kuppan <tkmagesh77@gmail.com>
2024-09-18 18:15:00 +0530

    react assignment:

--- node-intro/02-express-app/server.js
+++ node-intro/02-express-app/server.js
@@ -1,6 +1,7 @@
 import * as http from 'node:http';
 import express from 'express'
-
+import morgan from "morgan";
+import compression from 'compression'
 const app = express();
 
 const products = [
@@ -9,15 +10,20 @@ const products = [
   { id: 102, name: "Marker", cost: 50 },
 ];
 
+// compression middleware
+app.use(compression())
+
+// log middleware
+app.use(morgan('combined'))
+
+// static middleware
+app.use(express.static('public'))
+
 // json parsing middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 
-// log middleware
-app.use(function(req, res, next){
-    console.log(`[log] : ${req.method} - ${req.url}`);
-    next()
-})
+
 
 app.get("/products", function(req, res, next){
     // res.write(JSON.stringify(products));
@@ -28,7 +34,12 @@ app.get("/products", function(req, res, next){
     const desc = req.query.desc;
     if (sortBy){
         if (desc && desc === 'true'){
-            products.sort((p1, p2) => p2[sortBy] - p1[sortBy]);
+            // products.sort((p1, p2) => p2[sortBy] - p1[sortBy]);
+            products.sort((p1, p2) => {
+                if (p2[sortBy] > p1[sortBy]) return 1;
+                if (p2[sortBy] < p1[sortBy]) return -1;
+                return 0;
+            })
         } else {
             products.sort((p1, p2) => p1[sortBy] - p2[sortBy]);
         }

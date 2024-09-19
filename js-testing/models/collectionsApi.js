8811b9a58e2b802edd1338f17b926b24162318ff
Magesh Kuppan <tkmagesh77@gmail.com>
2024-09-19 17:04:47 +0530

    assignment - solution

--- js-testing/models/collectionsApi.js
+++ js-testing/models/collectionsApi.js
@@ -8,16 +8,54 @@ groupBy
 
 
 /* DO NOT USE builtin "array functions" */
-function sort(list, attrName){
+function sort(list, comparer){
+    let comparerFn;
+    if (typeof comparer !== 'string' && typeof comparer !== 'function')
+        throw new Error('Invalid comparer')
+    if (typeof comparer === 'string'){
+        comparerFn = function(i1, i2){
+            return i1[comparer] - i2[comparer]
+        }
+    } else {
+        comparerFn = comparer;
+    }
+    
     for(let i = 0; i < list.length-1; i++){
         for (let j = i+1; j < list.length; j++){
-            if (list[i][attrName] > list[j][attrName]){
+            if (comparerFn(list[i], list[j]) > 0){
                 [list[i], list[j]] = [list[j], list[i]]
             }
         }
     }
 }
 
-const collectionsApi = { sort }
+function filter(list, fn){
+    let result = []
+    for(let item of list){
+        if (fn(item)){
+            result.push(item)
+        }
+    }
+    return result
+}
+
+function max(list, fn){
+    let result = list[0]
+    for (let idx = 1; idx < list.length; idx++){
+        if (!fn(result, list[idx]))
+            result = list[idx]
+    }
+    return result
+}
+
+function min(list, fn) {
+  let result = list[0];
+  for (let idx = 1; idx < list.length; idx++) {
+    if (!fn(result, list[idx])) result = list[idx];
+  }
+  return result;
+}
+
+const collectionsApi = { sort, filter, max, min };
 
 module.exports = collectionsApi;
--- js-testing/models/collectionsApi.test.js
+++ js-testing/models/collectionsApi.test.js
@@ -10,33 +10,57 @@ describe("Collection API", () => {
           { id: 3, name: "Len", cost: 60, units: 60, category: "grocery" },
           { id: 5, name: "Zen", cost: 30, units: 30, category: "grocery" },
           { id: 1, name: "Ken", cost: 20, units: 80, category: "utencil" },
-          { id: 7, name: "Mouse", cost: 100, units: 20, category: "electronics",},
+          { id: 7, name: "Mouse", cost: 100, units: 20, category: "electronics"},
         ];
     })
     describe("Sort", () => {
         it("should sort the given collection by id", () => {
             // arrange
-            let firstProductId = 1,
-                lastProductId = 9;
+            let firstProduct = {
+                id: 1,
+                name: "Ken",
+                cost: 20,
+                units: 80,
+                category: "utencil",
+              },
+              lastProduct = {
+                id: 9,
+                name: "Ten",
+                cost: 70,
+                units: 70,
+                category: "stationary",
+              };
                 
             // act
             collectionsApi.sort(products, "id");
 
             // assert
-            expect(products[0].id).toBe(firstProductId)
-            expect(products[products.length-1].id).toBe(lastProductId)
+            expect(products[0]).toStrictEqual(firstProduct)
+            expect(products[products.length-1]).toStrictEqual(lastProduct)
         });
         it("should sort the given collection by cost", () => {
           // arrange
-          let firstProductId = 1,
-            lastProductId = 7;
+          let firstProduct = {
+              id: 1,
+              name: "Ken",
+              cost: 20,
+              units: 80,
+              category: "utencil",
+            },
+            lastProduct = {
+              id: 7,
+              name: "Mouse",
+              cost: 100,
+              units: 20,
+              category: "electronics",
+            };
 
           // act
           collectionsApi.sort(products, "cost");
 
           // assert
-          expect(products[0].id).toBe(firstProductId);
-          expect(products[products.length - 1].id).toBe(lastProductId);
+          expect(products[0]).toStrictEqual(firstProduct);
+          expect(products[products.length - 1]).toStrictEqual(lastProduct);
         });
 
         it("should sort the given collection by units", () => {
@@ -52,43 +76,114 @@ describe("Collection API", () => {
           expect(products[products.length - 1].id).toBe(lastProductId);
         });
 
-        it.skip("should sort the given collection by product value (cost * units) ", () => {
+        it("should sort the given collection by product value (cost * units) ", () => {
           // arrange
-          let firstProductId = 1,
-            lastProductId = 9;
-
+          let firstProduct = {
+              id: 5,
+              name: "Zen",
+              cost: 30,
+              units: 30,
+              category: "grocery",
+            },
+            lastProduct = {
+              id: 9,
+              name: "Ten",
+              cost: 70,
+              units: 70,
+              category: "stationary",
+            };
+          const productsComparerByValue = (p1, p2) => {
+            const p1Value = p1.cost * p1.units,
+                p2Value = p2.cost * p2.units;
+            if (p1Value > p2Value) return 1;
+            if (p1Value < p2Value) return -1;
+            return 0
+          }
           // act
-          collectionsApi.sort(products, /* ? */);
+          
+          collectionsApi.sort(products, productsComparerByValue);
 
           // assert
-          expect(products[0].id).toBe(firstProductId);
-          expect(products[products.length - 1].id).toBe(lastProductId);
+          expect(products[0]).toStrictEqual(firstProduct); // for deep comparison
+          expect(products[products.length - 1]).toStrictEqual(lastProduct);
         });
+
+        it("should throw error for invalid argument", () => {
+            expect(() => {
+                collectionsApi.sort(products)
+            }).toThrow()
+        })
     });
 
-    describe.skip("Filter", () => {
+    describe("Filter", () => {
         it("Should be able to filter stationary products (category == 'stationary')", () => {
-            
+            // act
+            const isStationaryProduct = (product) => product.category === 'stationary';
+            const stationaryProducts = collectionsApi.filter(
+              products,
+              isStationaryProduct
+            );
+          
+            // assert
+            expect(stationaryProducts).toHaveLength(2)
+            expect(stationaryProducts[0].category).toBe('stationary')
         })
 
         it("Should be able to filter costly products (cost > 50)", () => {
+          // act
+          const isCostlyProduct = product => product.cost > 50;
+          const costlyProducts = collectionsApi.filter(
+            products,
+            isCostlyProduct
+          );
 
+          // assert
+          expect(costlyProducts).toHaveLength(3);
+          expect(costlyProducts[0].cost).toBeGreaterThan(50);
         });
 
         it("Should be able to filter understocked products (units < 50)", () => {
+          // act
+          const isUnderstockedProduct = (product) => product.units < 50;
+          const understockedProducts = collectionsApi.filter(
+            products,
+            isUnderstockedProduct
+          );
 
+          // assert
+          expect(understockedProducts).toHaveLength(3);
+          expect(understockedProducts[0].units).toBeLessThan(50);
         });
     });
 
-    describe.skip("Max", () => {
+    describe("Max", () => {
         it("Should return the costliest product", () => {
+
+            const expected = {
+              id: 7,
+              name: "Mouse",
+              cost: 100,
+              units: 20,
+              category: "electronics",
+            };
+
+            const isGreaterByCost = (p1, p2) => p1.cost > p2.cost;
+            const costliest = collectionsApi.max(products, isGreaterByCost)
+
+            expect(costliest).toStrictEqual(expected)
+
 
         })
     })
 
-    describe.skip("Min", () => {
+    describe("Min", () => {
       it("Should return the cheapest product", () => {
+            const expected = { id: 1, name: "Ken", cost: 20, units: 80, category: "utencil" };
 
+            const isCheaperByCost = (p1, p2) => p1.cost < p2.cost;
+            const cheapest = collectionsApi.min(products, isCheaperByCost);
+
+            expect(cheapest).toStrictEqual(expected);
       });
     });
 
